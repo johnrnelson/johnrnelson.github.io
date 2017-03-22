d119d5b351336d1dd28ee4fb630c04995cdc6d2d
John Nelson <me@johnrnelson.com>
2017-03-22 21:45:06 +0000

    Server commit by bash script...

---
+++ html/js/main.js
@@ -0,0 +1,32 @@
+window.APP = {
+
+};
+
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+
+window.onload = function() {
+
+
+    APP.DEBUGEL = document.getElementById('DEBUGEL');
+    APP.DEBUGEL.innerHTML = 'HOK';
+    
+
+    if (localStorage.getItem('DEBUG')) {
+        window.AppScriptExtName = '';
+        console.info("%c%s", "color: red;  font-size: 14px;", "You are in debug mode at level[" + localStorage.getItem('DEBUG') + "]. To clear your settings use this at the console...\r\n\r\n" + "\tAPP.DEBUG.Set(0);\r\n\r\n");
+    }
+    else {
+        window.AppScriptExtName = '-min';
+    }
+
+    require.config({
+        baseUrl: 'js/desktop'
+    });
+
+
+
+    //  require(["APP" + window.AppScriptExtName], function() {});
+
+}
