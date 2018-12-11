2ae809fc2cbd3a10697599d2c48d20852a0d28cc
John Nelson <me@johnrnelson.com>
2018-12-11 08:52:24 -0800

    styles and formatting

--- resume/resume.js
+++ resume/resume.js
@@ -2,19 +2,16 @@
     Put some mojo in the resume... :-)
 */
 window.onload = function () {
-    console.log('ok ready for dates-->', moment);
-
+ 
+    /*
+        Show the years for the dates in the work history...
+    */
     const wrkDates = document.querySelectorAll('.HistoryYear');
+ 
     for (let index = 0; index < wrkDates.length; index++) {
         const wrkDt = wrkDates[index];
         const dtsAry = wrkDt.innerText.split("-");
-        const dispDate = "1/1/" + dtsAry[1];
-
-        console.log(wrkDt.innerText, dispDate);
-
-        wrkDt.title = "" + moment(dispDate).fromNow()
-
-
+        const dispDate = "1/1/" + dtsAry[1]; 
+        wrkDt.title = "" + moment(dispDate).fromNow();
     }
-
 };
