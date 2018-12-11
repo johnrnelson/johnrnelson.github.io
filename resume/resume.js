bfe2dd9eae98e76dad0e9c318b0f36937a0e4db7
John Nelson <me@johnrnelson.com>
2018-12-11 08:36:33 -0800

    formating and moment dates

--- resume/resume.js
+++ resume/resume.js
@@ -1,7 +1,20 @@
 /*
     Put some mojo in the resume... :-)
 */
-window.onload = function(){
-    console.log('ok ready for dates-->',moment);
+window.onload = function () {
+    console.log('ok ready for dates-->', moment);
+
+    const wrkDates = document.querySelectorAll('.HistoryYear');
+    for (let index = 0; index < wrkDates.length; index++) {
+        const wrkDt = wrkDates[index];
+        const dtsAry = wrkDt.innerText.split("-");
+        const dispDate = "1/1/" + dtsAry[1];
+
+        console.log(wrkDt.innerText, dispDate);
+
+        wrkDt.title = "" + moment(dispDate).fromNow()
+
+
+    }
 
 };
