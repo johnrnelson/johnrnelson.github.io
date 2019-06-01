f4a330f127dd33cfebdc29a330e3347d7d4195bd
John Nelson <me@johnrnelson.com>
2019-06-01 09:50:23 -0700

    date fix

--- resume/resume.js
+++ resume/resume.js
@@ -2,17 +2,23 @@
     Put some mojo in the resume... :-)
 */
 window.onload = function () {
- 
+
     /*
         Show the years for the dates in the work history...
     */
     const wrkDates = document.querySelectorAll('.HistoryYear');
  
+
     for (let index = 0; index < wrkDates.length; index++) {
-        const wrkDt = wrkDates[index];
-        const dtsAry = wrkDt.innerText.split('-');                
-        if(index>0){
-             wrkDt.innerHTML += '<span style="color:gray"> (' + moment(new Date('01/01/' + dtsAry[1])).fromNow() + ')<span>';
+        try {
+            const wrkDt = wrkDates[index];
+            const dtsAry = wrkDt.innerText.split('-');
+            if (index > 0) {
+                wrkDt.innerHTML += '<span style="color:gray"> (' + moment(new Date('01/01/' + dtsAry[1])).fromNow() + ')<span>';
+            }
+        } catch (errDate) {
+            //Today is not a date so just ignore...
         }
+
     }
 };
